b86a4869ee2d89483fcf77f7b8d3a068cbd33ff2
Ashlin Eldridge <ashlineldridge@users.noreply.github.com>
2021-10-20 05:59:48 +1000

    Day08: Part 1 and Part 2 - pretty happy with result.

--- day08/src/main.rs
+++ day08/src/main.rs
@@ -8,20 +8,52 @@ fn main() -> Result<()> {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input)?;
 
-    let values: Result<VecDeque<u32>, _> = input
-        .trim()
-        .split(' ')
-        .map(|v| v.parse::<u32>())
-        .collect();
+    let values: Result<VecDeque<u32>, _> =
+        input.trim().split(' ').map(|v| v.parse::<u32>()).collect();
     let mut values = values?;
+    let root = build_tree(&mut values)?;
 
-    let answer = part1(&mut values)?;
-    println!("Answer to part 1 is {}", answer);
+    println!("Answer to part 1 is {}", root.value1());
+    println!("Answer to part 2 is {}", root.value2());
 
     Ok(())
 }
 
-fn part1(values: &mut VecDeque<u32>) -> Result<u32> {
+struct Node {
+    children: Vec<Node>,
+    entries: Vec<u32>,
+}
+
+impl Node {
+    fn new() -> Node {
+        Node {
+            children: vec![],
+            entries: vec![],
+        }
+    }
+
+    fn value1(&self) -> u32 {
+        self.children.iter().fold(0, |acc, n| acc + n.value1()) + self.entries.iter().sum::<u32>()
+    }
+
+    fn value2(&self) -> u32 {
+        if self.children.is_empty() {
+            return self.entries.iter().sum();
+        }
+
+        let mut value = 0;
+        for entry in &self.entries {
+            if *entry > 0 {
+                let index = entry.saturating_sub(1) as usize;
+                value += self.children.get(index).map(|n| n.value2()).unwrap_or(0);
+            }
+        }
+
+        value
+    }
+}
+
+fn build_tree(values: &mut VecDeque<u32>) -> Result<Node> {
     let total_children = values
         .pop_front()
         .context("expected header specifying number of child nodes")?;
@@ -30,12 +62,13 @@ fn part1(values: &mut VecDeque<u32>) -> Result<u32> {
         .context("expected header specifying number of metadata entries")?
         as usize;
 
-    let mut result = 0;
+    let mut node = Node::new();
     for _ in 0..total_children {
-        result += part1(values)?;
+        let child = build_tree(values)?;
+        node.children.push(child);
     }
 
-    let entries = values.drain(0..total_entries).collect::<Vec<u32>>();
+    let mut entries = values.drain(0..total_entries).collect::<Vec<u32>>();
     if entries.len() != total_entries {
         bail!(
             "expected {} metadata entries but got {}",
@@ -44,5 +77,7 @@ fn part1(values: &mut VecDeque<u32>) -> Result<u32> {
         );
     }
 
-    Ok(result + entries.iter().sum::<u32>())
+    node.entries.append(&mut entries);
+
+    Ok(node)
 }
