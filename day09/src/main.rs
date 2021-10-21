e76d106e0ac9a2689e1119ab599b7a1cd9fbc541
Ashlin Eldridge <ashlineldridge@users.noreply.github.com>
2021-10-22 08:00:48 +1000

    Day09: Optimised to use a vec with back/prev indices. Much faster.

--- day09/src/main.rs
+++ day09/src/main.rs
@@ -1,9 +1,9 @@
-use anyhow::{anyhow, Result};
+use anyhow::{anyhow, bail, Context, Result};
 use lazy_static::lazy_static;
 use regex::Regex;
 use std::{
-    collections::{HashMap, VecDeque},
-    io::{self, stdout, Read, Write},
+    collections::HashMap,
+    io::{self, Read},
     str::FromStr,
 };
 
@@ -11,57 +11,134 @@ fn main() -> Result<()> {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input)?;
 
-    let games = input
+    let mut games = input
         .lines()
         .map(|v| v.parse())
         .collect::<Result<Vec<Game>>>()?;
 
-    for (num, game) in games.iter().enumerate() {
-        let score = high_score(game)?;
-        println!("Game {}: High score is {}", num + 1, score);
+    for (num, game) in games.iter_mut().enumerate() {
+        let (winner_id, score) = game.run()?;
+        println!(
+            "Game {}: Player {} won with high score is {}",
+            num + 1,
+            winner_id,
+            score
+        );
     }
 
     Ok(())
 }
 
-fn high_score(game: &Game) -> Result<u32> {
-    let mut circle: VecDeque<u32> = VecDeque::new();
-    let mut current_marble_index: usize = 0;
-    let mut current_player: u32 = 1;
-    let mut points_by_player: HashMap<u32, u32> = HashMap::new();
-    let mut percent_complete: u8 = 0;
-    circle.push_back(0);
+#[derive(Clone, Debug)]
+struct Marble {
+    value: MarbleValue,
+    next_index: usize,
+    prev_index: usize,
+}
 
-    for turn in 1..game.total_marbles {
-        if turn % 23 == 0 {
-            let i = current_marble_index as i32 - 7;
-            let n = circle.len() as i32;
-            current_marble_index = (((i % n) + n) % n) as usize;
-            let points = turn + circle.remove(current_marble_index).unwrap();
-            *points_by_player.entry(current_player).or_default() += points;
-        } else {
-            current_marble_index = (current_marble_index + 2) % circle.len();
-            circle.insert(current_marble_index, turn);
-        }
+type MarbleValue = u32;
 
-        current_player = (current_player % game.total_players) + 1;
+type Player = u32;
 
-        let new_percent_complete = (turn * 100 / (game.total_marbles - 1)) as u8;
-        if new_percent_complete > percent_complete {
-            percent_complete = new_percent_complete;
-            print!("\rCompleted {}%", percent_complete);
-            stdout().flush().unwrap();
+type Score = HashMap<Player, u32>;
+
+struct Game {
+    total_players: usize,
+    total_marbles: usize,
+    circle: Vec<Marble>,
+    current_marble_index: usize,
+    next_marble_value: MarbleValue,
+    next_player: Player,
+    score: Score,
+}
+
+impl Game {
+    fn new(total_players: usize, total_marbles: usize) -> Game {
+        Game {
+            total_players,
+            total_marbles,
+            circle: vec![Marble {
+                value: 0,
+                next_index: 0,
+                prev_index: 0,
+            }],
+            current_marble_index: 0,
+            next_marble_value: 1,
+            next_player: 1,
+            score: Score::new(),
         }
     }
 
-    println!();
+    fn run(&mut self) -> Result<(Player, u32)> {
+        if self.total_marbles == 0 {
+            bail!("Game can't be played without marbles");
+        }
+        if self.total_players == 0 {
+            bail!("Game can't be played without players");
+        }
 
-    Ok(*points_by_player.values().max().unwrap_or(&0))
-}
+        while self.play_next() {}
 
-struct Game {
-    total_players: u32,
-    total_marbles: u32,
+        self.score
+            .iter()
+            .max_by_key(|(_, v)| **v)
+            .map(|(k, v)| (*k, *v))
+            .context("Game did not produce a winner")
+    }
+
+    fn play_next(&mut self) -> bool {
+        if self.next_marble_value == self.total_marbles as u32 {
+            return false;
+        }
+
+        if self.next_marble_value % 23 == 0 {
+            let prev_7 = self.prev(7).clone();
+            self.prev(8).next_index = prev_7.next_index;
+            self.prev(6).prev_index = prev_7.prev_index;
+            self.current_marble_index = prev_7.next_index;
+
+            let points = (self.next_marble_value + prev_7.value) as u32;
+            *self.score.entry(self.next_player).or_default() += points;
+        } else {
+            let next_marble = Marble {
+                value: self.next_marble_value,
+                next_index: self.next(1).next_index,
+                prev_index: self.curr().next_index,
+            };
+            let next_marble_index = self.circle.len();
+
+            self.next(2).prev_index = next_marble_index;
+            self.next(1).next_index = next_marble_index;
+
+            self.circle.push(next_marble);
+            self.current_marble_index = next_marble_index;
+        }
+
+        self.next_marble_value += 1;
+        self.next_player = (self.next_player % self.total_players as u32) + 1;
+
+        true
+    }
+
+    fn curr(&mut self) -> &mut Marble {
+        self.circle.get_mut(self.current_marble_index).unwrap()
+    }
+
+    fn next(&mut self, n: usize) -> &mut Marble {
+        let mut next_index = self.current_marble_index;
+        for _ in 0..n {
+            next_index = self.circle[next_index].next_index;
+        }
+        self.circle.get_mut(next_index).unwrap()
+    }
+
+    fn prev(&mut self, n: usize) -> &mut Marble {
+        let mut prev_index = self.current_marble_index;
+        for _ in 0..n {
+            prev_index = self.circle[prev_index].prev_index;
+        }
+        self.circle.get_mut(prev_index).unwrap()
+    }
 }
 
 impl FromStr for Game {
@@ -79,9 +156,6 @@ impl FromStr for Game {
             .captures(s)
             .ok_or_else(|| anyhow!("Invalid line: {}", s))?;
 
-        Ok(Game {
-            total_players: caps["players"].parse()?,
-            total_marbles: caps["points"].parse::<u32>()? + 1,
-        })
+        Ok(Game::new(caps["players"].parse()?, caps["points"].parse::<usize>()? + 1))
     }
 }
