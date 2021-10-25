2bb9516ecaa93ec1cdccde07db433ad784dbc444
Ashlin Eldridge <ashlineldridge@users.noreply.github.com>
2021-10-26 06:55:10 +1000

    Day12: Initial structure.

---
+++ day12/src/main.rs
@@ -0,0 +1,108 @@
+use anyhow::{anyhow, Context, Result};
+use lazy_static::lazy_static;
+use regex::Regex;
+use std::{
+    io::{self, Read},
+    str::FromStr,
+};
+
+fn main() -> Result<()> {
+    let mut input = String::new();
+    io::stdin().read_to_string(&mut input)?;
+
+    let plant_state: PlantState = input.parse()?;
+    println!("Read {:?}", plant_state);
+
+    Ok(())
+}
+
+#[derive(Debug)]
+struct PlantState {
+    state: Vec<bool>,
+    rules: Vec<PlantRule>,
+}
+
+impl PlantState {
+    // fn advance(&mut self) {
+    // for (i, v) in self.state.iter_mut().enumerate() {
+    // for r in &self.rules {
+    // if let Some(outcome) = r.eval(self.state, i) {}
+    // }
+    // }
+    // }
+}
+
+impl FromStr for PlantState {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let lines = s.lines().collect::<Vec<&str>>();
+        let state = lines
+            .get(0)
+            .and_then(|line| line.split_whitespace().last())
+            .map(|pattern| pattern.chars().map(|c| c == '#').collect::<Vec<bool>>())
+            .context("Invalid input")?;
+        let rules: Result<Vec<PlantRule>> = lines[1..].iter().filter_map(|line| {
+            if line.is_empty() {
+                None
+            } else {
+                Some(line.parse())
+            }
+        }).collect();
+        let rules = rules?;
+
+        Ok(PlantState { state, rules })
+    }
+}
+
+#[derive(Debug)]
+struct PlantRule {
+    pattern: Vec<bool>,
+    outcome: bool,
+}
+
+impl PlantRule {
+    fn matches(&self, state: &[bool], index: i32) -> bool {
+        let siblings = self.pattern.len() as i32 / 2;
+        for i in index - siblings..=index + siblings {
+            let mut s = false;
+            if i >= 0 && i < state.len() as i32 {
+                s = state[i as usize];
+            }
+
+            if self.pattern[(i - index + siblings) as usize] != s {
+                return false;
+            }
+        }
+
+        true
+    }
+
+    fn eval(&self, state: &[bool], index: i32) -> Option<bool> {
+        if self.matches(state, index) {
+            Some(self.outcome)
+        } else {
+            None
+        }
+    }
+}
+
+impl FromStr for PlantRule {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        lazy_static! {
+            static ref RE: Regex =
+                Regex::new(r"^(?P<pattern>[#\.]{5}) => (?P<outcome>#|.)$").unwrap();
+        }
+
+        let caps = RE
+            .captures(s)
+            .ok_or_else(|| anyhow!("Invalid line: {}", s))?;
+
+        Ok(PlantRule {
+            pattern: caps["pattern"].chars().map(|c| c == '#').collect(),
+            outcome: &caps["outcome"] == "#",
+        })
+    }
+}
