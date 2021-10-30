f66b44a5cf84d96e57a9a5888bbfcfb6ca56625c
Ashlin Eldridge <ashlineldridge@users.noreply.github.com>
2021-10-30 13:45:57 +1000

    Day12: Part2 answer deduction.

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -12,168 +12,123 @@ use std::{
 // The spread factor specifies the number of plant siblings that have an influence on
 // the next generation of plants. The total rule pattern width = 2 * spread + 1 (i.e.,
 // the siblings on either side of the current plant plus the current plant itself).
-const SPREAD_FACTOR: usize = 2;
+const SPREAD_FACTOR: u32 = 2;
 
-const TOTAL_RULES: usize = 2_usize.pow((2 * SPREAD_FACTOR + 1) as u32);
+const RULE_WIDTH: u32 = 2 * SPREAD_FACTOR + 1;
+
+const TOTAL_RULES: usize = 2_u32.pow(RULE_WIDTH) as usize;
 
 fn main() -> Result<()> {
-    // let left_row: Vec<u8> = vec![0b01101011, 0b10101011, 0b01111010];
-    // let right_row: Vec<u8> = vec![0b01101011, 0b10101011, 0b01111010];
-    // let nursery = Nursery {
-    //     negative_plants: left_row,
-    //     positive_plants: right_row,
-    //     rules: [0; TOTAL_RULES],
-    // };
-
-    // println!("{:08b}", nursery.plant_group(-17));
-
     let mut input = String::new();
     io::stdin().read_to_string(&mut input)?;
 
-    let mut nursery: Nursery = input.parse()?;
-    // println!("{}", nursery);
-    run_sim(&mut nursery, 4);
+    // Even after optimizing this to use bit manipulation rather than hashsets, etc,
+    // 50,000,000,000 iterations was still gonna take longer than I could be bothered
+    // waiting around. Running the below numbers produces the following pattern:
+    //
+    // After 500 generations the answer is: 21684
+    // After 5000 generations the answer is: 201684
+    // After 50000 generations the answer is: 2001684
+    //
+    // So, for 5XXX generations, where XXX is some number of zeros greater than two, the
+    // answer is 2YYY1684 where YYYY = the number of zeros in XXX - 2. The answer for
+    // 50,000,000,000 generations is then 2,000,000,001,684.
+
+    let nursery: Nursery = input.parse()?;
+    run_sim(nursery.clone(), 500);
+    run_sim(nursery.clone(), 5_000);
+    run_sim(nursery, 50_000);
 
     Ok(())
 }
 
-fn run_sim(nursery: &mut Nursery, count: usize) {
-    println!("0: {}", nursery);
-    for i in 0..count {
+fn run_sim(mut nursery: Nursery, count: usize) {
+    for _ in 0..count {
         nursery.advance();
-        println!("{}: {}", i + 1, nursery);
     }
 
     let sum = nursery.sum();
     println!("After {} generations the answer is: {}", count, sum);
 }
 
-#[derive(Debug)]
+#[derive(Clone)]
 struct Nursery {
-    negative_plants: Vec<u8>,
-    positive_plants: Vec<u8>,
+    plants: Vec<u8>,
+    plant_zero_index: usize,
     rules: [u8; TOTAL_RULES],
 }
 
 impl Nursery {
-    fn advance(&mut self) {
-        let mut next_negative_plants = vec![];
-        for i in 0..self.negative_plants.len() {
-            let mut byte = 0;
-            for bit in 0..u8::BITS {
-                let plant_id = -(i as i32 * u8::BITS as i32 + bit as i32);
-                let plant_group = self.plant_group(plant_id);
-                let outcome = self.rules[plant_group as usize];
-
-                if outcome > 0 {
-                    byte |= 0x01 << bit;
-                }
-                println!("ID: {}, Group: {:05b}, Outcome: {}, Byte {}: {:08b}", plant_id, plant_group, outcome, i, byte);
-
-            }
-
-            next_negative_plants.push(byte);
+    fn new(mut plants: Vec<u8>, rules: [u8; TOTAL_RULES]) -> Nursery {
+        let mut plant_zero_index = 0;
+        if *plants.first().unwrap_or(&0) != 0 {
+            plants.insert(0, 0);
+            plant_zero_index += 1;
         }
 
-        self.negative_plants = next_negative_plants;
-
-        let mut next_positive_plants = vec![];
-        for i in 0..self.positive_plants.len() {
-            let mut byte = 0;
-            for bit in 0..u8::BITS {
-                let plant_id = i as i32 * u8::BITS as i32 + bit as i32;
-                let plant_group = self.plant_group(plant_id);
-                let outcome = self.rules[plant_group as usize];
-
-                if outcome > 0 {
-                    // println!("Shifting by: {} for {}", u8::BITS - bit - 1, bit);
-                    byte |= 0x01 << (u8::BITS - bit - 1);
-                }
-                // println!("ID: {}, Group: {:05b}, Outcome: {}, Byte {}: {:08b}", plant_id, plant_group, outcome, i, byte);
-            }
-
-            next_positive_plants.push(byte);
-        }
-
-        self.positive_plants = next_positive_plants;
-    }
-
-    fn plant_group(&self, id: i32) -> u8 {
-        let bit_range = (id - SPREAD_FACTOR as i32, id + SPREAD_FACTOR as i32);
-        let byte_range = (bit_range.0 / u8::BITS as i32, bit_range.1 / u8::BITS as i32);
-
-        // println!("plant_group: id: {}, bit_range: {:?}, byte_range: {:?}", id, bit_range, byte_range);
-
-        let left_byte = if bit_range.0 < 0 {
-            self.negative_plants
-                .get(byte_range.0.abs() as usize)
-                .unwrap_or(&0)
-                .reverse_bits()
-        } else {
-            *self.positive_plants.get(byte_range.0 as usize).unwrap_or(&0)
-        };
-
-        let (right_byte, right_shift) = if bit_range.1 < 0 {
-            (
-                self.negative_plants
-                    .get(byte_range.1.abs() as usize)
-                    .unwrap_or(&0)
-                    .reverse_bits(),
-                bit_range.1.abs() as u32 % u8::BITS,
-            )
-        } else {
-            (
-                *self.positive_plants.get(byte_range.1 as usize).unwrap_or(&0),
-                u8::BITS - (bit_range.1.abs() as u32 % u8::BITS) - 1,
-            )
-        };
-
-        let mut plant_group = (left_byte as u16) << u8::BITS | right_byte as u16;
-        plant_group >>= right_shift;
-        plant_group &= 0xFF >> (u8::BITS as usize - (2 * SPREAD_FACTOR + 1));
-
-        plant_group as u8
+        Nursery { plants, plant_zero_index, rules }
     }
 
     fn sum(&self) -> i32 {
         let mut sum = 0;
-        for (i, b) in self.positive_plants.iter().enumerate() {
-            let mut b = *b;
+        for (i, byte) in self.plants.iter().enumerate() {
             for bit in 0..u8::BITS {
-                if b & 0x01 == 1 {
-                    sum += i as i32 * u8::BITS as i32 + bit as i32;
+                if byte & (0x01 << bit) != 0 {
+                    let plant_id = (i as i32 - self.plant_zero_index as i32) * u8::BITS as i32
+                        + (u8::BITS - bit - 1) as i32;
+                    sum += plant_id;
                 }
-                b >>= 1;
-            }
-        }
-
-        for (i, b) in self.negative_plants.iter().enumerate() {
-            let mut b = *b;
-            for bit in 0..u8::BITS {
-                if b & 0x01 == 1 {
-                    sum += -(i as i32 * u8::BITS as i32 + bit as i32);
-                }
-                b >>= 1;
             }
         }
 
         sum
+    }
+
+    fn advance(&mut self) {
+        if *self.plants.last().unwrap_or(&0) != 0 {
+            self.plants.push(0);
+        }
+
+        let mut new_plants = Vec::with_capacity(self.plants.len() + 2);
+        if *self.plants.first().unwrap_or(&0) != 0 {
+            new_plants.push(0);
+            self.plant_zero_index += 1;
+        }
+
+        let mut prev_byte = 0;
+        for i in 0..self.plants.len() {
+            let this_byte = self.plants[i];
+            let next_byte = *self.plants.get(i + 1).unwrap_or(&0);
+            let mut new_byte = 0;
+
+            let byte_group = (prev_byte as u32) << (u8::BITS * 2)
+                | (this_byte as u32) << u8::BITS
+                | next_byte as u32;
+
+            for bit in 0..u8::BITS {
+                let bit_index = i as u32 * u8::BITS + bit;
+                let max_bit_index = (i as u32 + 2) * u8::BITS - 1;
+                let shift_by = max_bit_index - bit_index - SPREAD_FACTOR;
+                let rule_id = (byte_group >> shift_by) as u8 & !(0xFF << RULE_WIDTH);
+                let outcome = self.rules[rule_id as usize];
+
+                if outcome > 0 {
+                    new_byte |= 0x01 << (u8::BITS - bit - 1);
+                }
+            }
+
+            new_plants.push(new_byte);
+            prev_byte = this_byte;
+        }
+
+        self.plants = new_plants;
     }
 }
 
 impl Display for Nursery {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         let mut out = String::from("");
-
-        for (i, b) in self.negative_plants.iter().enumerate().rev() {
-            if i == 0 {
-                out += format!("{:07b}", b >> 1).as_str();
-            } else {
-                out += format!("{:08b}", b).as_str();
-            }
-        }
-
-        for b in &self.positive_plants {
+        for b in &self.plants {
             out += format!("{:08b}", b).as_str();
         }
 
@@ -211,13 +166,8 @@ impl FromStr for Nursery {
             rules[r.0 as usize] = r.1;
         }
 
-        Ok(Nursery {
-            negative_plants: vec![0],
-            positive_plants: initial_plants,
-            rules,
-        })
+        Ok(Nursery::new(initial_plants, rules))
     }
-
 }
 
 fn str_to_byte_vec(pattern: &str) -> Result<Vec<u8>> {
@@ -235,31 +185,6 @@ fn str_to_byte_vec(pattern: &str) -> Result<Vec<u8>> {
 }
 
 fn str_to_byte(pattern: &str) -> Result<u8> {
-    let byte = pattern
-        .replace("#", "1")
-        .replace(".", "0");
+    let byte = pattern.replace("#", "1").replace(".", "0");
     Ok(u8::from_str_radix(&byte, 2)?)
-}
-
-
-#[cfg(test)]
-mod test {
-    use super::*;
-
-    #[test]
-    fn plant_group() {
-        let left_row: Vec<u8> = vec![0b01101011, 0b10101011, 0b01111010];
-        let right_row: Vec<u8> = vec![0b01101011, 0b10101011, 0b01111010];
-        let nursery = Nursery {
-            negative_plants: left_row,
-            positive_plants: right_row,
-            rules: [0; TOTAL_RULES],
-        };
-
-        println!("{:08b}", nursery.plant_group(-17));
-        assert_eq!(nursery.plant_group(8), 0b11101);
-        assert_eq!(nursery.plant_group(0), 0b11011);
-        assert_eq!(nursery.plant_group(-9), 0b01011);
-        assert_eq!(nursery.plant_group(-17), 0b11101);
-    }
 }
