3d675f946acbb388489ce8ae12324f9b7b0ad176
Ashlin Eldridge <ashlineldridge@users.noreply.github.com>
2021-11-17 11:32:48 +1000

    Day15: Finally got there.

--- day15/src/main.rs
+++ day15/src/main.rs
@@ -9,17 +9,8 @@ fn main() -> Result<()> {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input)?;
 
-    let mut game: Game = input.parse()?;
-    let shortest_paths = game.shortest_paths(Point::new(4, 1));
-    println!("Calculated {} shortest paths", shortest_paths.len());
-
-    println!("{}", game);
-    for point in shortest_paths.keys() {
-        game.grid.insert(*point, Object::Marker('?'));
-    }
-    println!("{}", game);
-
-    // part1(input)?;
+    part1(input.clone())?;
+    part2(input)?;
 
     Ok(())
 }
@@ -27,13 +18,47 @@ fn main() -> Result<()> {
 fn part1(input: String) -> Result<()> {
     let mut game: Game = input.parse()?;
     loop {
-        match game.play_round() {
-            GameOutcome::InProgress(_) => (),
-            GameOutcome::Complete(score) => {
-                println!("Game complete: {:?}", score);
-                break;
-            },
+        if let GameState::Complete(score) = game.play_round() {
+            println!("Game complete: {:?}", score);
+            println!("Game outcome: {}", score.outcome());
+            break;
         }
+    }
+
+    Ok(())
+}
+
+fn part2(input: String) -> Result<()> {
+    let game: Game = input.parse()?;
+    let initial_elf_count = game.outcome().score().remaining_combatants[&CombatantKind::Elf];
+    let mut attack_power = Combatant::DEFAULT_ATTACK_POWER + 1;
+
+    loop {
+        let mut game = game.clone();
+
+        let attack_powers = HashMap::from([
+            (CombatantKind::Elf, attack_power),
+            (CombatantKind::Goblin, Combatant::DEFAULT_ATTACK_POWER),
+        ]);
+
+        game.update_attack_power(attack_powers);
+
+        let score = loop {
+            if let GameState::Complete(score) = game.play_round() {
+                break score;
+            }
+        };
+
+        println!("Elf attack power {}: {:?}", attack_power, score);
+
+        let final_elf_count = score.remaining_combatants[&CombatantKind::Elf];
+        if final_elf_count == initial_elf_count {
+            // The elves finally won without losing any combatants!
+            println!("Final game outcome: {}", score.outcome());
+            break;
+        }
+
+        attack_power += 1;
     }
 
     Ok(())
@@ -50,6 +75,15 @@ struct Combatant {
 enum CombatantKind {
     Elf,
     Goblin,
+}
+
+impl CombatantKind {
+    fn enemy(&self) -> CombatantKind {
+        match self {
+            CombatantKind::Elf => CombatantKind::Goblin,
+            CombatantKind::Goblin => CombatantKind::Elf,
+        }
+    }
 }
 
 impl Combatant {
@@ -73,21 +107,20 @@ impl Combatant {
     }
 
     fn is_enemy(&self, other: &Combatant) -> bool {
-        self.kind != other.kind
+        other.kind == self.kind.enemy()
     }
 }
 
-#[derive(PartialEq, Eq)]
+#[derive(Clone, PartialEq, Eq)]
 enum Object {
     Combatant(Combatant),
     Wall,
     Empty,
-    Marker(char),
 }
 
 impl Object {
     fn is_empty(&self) -> bool {
-        matches!(*self, Object::Empty | Object::Marker(_))
+        matches!(*self, Object::Empty)
     }
 
     fn is_enemy(&self, other: &Object) -> bool {
@@ -101,7 +134,14 @@ impl Object {
         matches!(self, Object::Combatant { .. })
     }
 
-    fn unwrap(&self) -> &Combatant {
+    fn as_combatant(&self) -> &Combatant {
+        match self {
+            Object::Combatant(c) => c,
+            _ => panic!("expected combatant but got: {}", self),
+        }
+    }
+
+    fn as_combatant_mut(&mut self) -> &mut Combatant {
         match self {
             Object::Combatant(c) => c,
             _ => panic!("expected combatant but got: {}", self),
@@ -130,7 +170,6 @@ impl Display for Object {
             Object::Combatant(_) => 'G',
             Object::Wall => '#',
             Object::Empty => '.',
-            Object::Marker(ch) => *ch,
         };
 
         f.write_char(ch)
@@ -174,21 +213,11 @@ impl PartialOrd for Point {
     }
 }
 
+#[derive(Clone)]
 struct Game {
     grid: HashMap<Point, Object>,
     width: usize,
     height: usize,
-    rounds: usize,
-}
-
-enum GameOutcome {
-    InProgress(Score),
-    Complete(Score),
-}
-
-#[derive(Debug)]
-struct Score {
-    result: HashMap<CombatantKind, usize>,
     rounds: usize,
 }
 
@@ -204,8 +233,8 @@ impl Game {
             .collect::<Vec<_>>();
 
         points.sort_unstable_by(|p1, p2| {
-            let c1 = self.grid[p1].unwrap();
-            let c2 = self.grid[p2].unwrap();
+            let c1 = self.grid[p1].as_combatant();
+            let c2 = self.grid[p2].as_combatant();
             match c1.health.cmp(&c2.health) {
                 std::cmp::Ordering::Equal => p1.cmp(p2),
                 v => v,
@@ -263,62 +292,97 @@ impl Game {
         points
     }
 
-    fn shortest_paths(&self, from: Point) -> HashMap<Point, Vec<Point>> {
+    // Uses Dijkstra's algorithm to calculate the shortest path from the specified point to every
+    // free point on the grid. See: https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm.
+    fn shortest_paths(&mut self, from: Point) -> HashMap<Point, Vec<Point>> {
         let mut shortest_paths = HashMap::new();
+
+        // The shortest path from the starting point to itself is an empty path.
         shortest_paths.insert(from, vec![]);
 
+        // Keep track of all the empty points on the grid which we have not yet visited. Visiting
+        // a point means that we have calculated the shortest path to that point and we will not
+        // consider it again.
         let mut unvisited = self.empty_points().into_iter().collect::<HashSet<_>>();
+
+        // Set the current point to the starting point and loop until we have calculated the
+        // shortest path to every free point which can be reached (some may be blocked).
         let mut current = from;
-
         loop {
-            println!("shortest_path: {:?}", current);
-
+            // The neighbors of the current point which are empty.
             let empty_neighbors = self
                 .empty_in_range(current)
                 .into_iter()
                 .collect::<HashSet<_>>();
 
+            // The neighbors of the current point which are empty and unvisited.
             let unvisited_neighbors = empty_neighbors
                 .intersection(&unvisited)
                 .cloned()
                 .collect::<HashSet<_>>();
 
+            // The path that we took to reach the current point.
             let current_path = shortest_paths[&current].clone();
-            let current_cost = current_path.len();
-            let neighbor_cost = current_cost + 1;
 
-            let mut possible_paths = vec![];
+            // The distance from the starting position to the current point.
+            let current_distance = current_path.len();
 
+            // The distance from the starting position to each neighbor of the current point.
+            let neighbor_distance = current_distance + 1;
+
+            // For each unvisited neighbor of the current point check whether the distance of the
+            // path to the neighbor that runs through the current point is less than any previously
+            // calculated tentative distance (i.e., the length of the path that we previously
+            // calculated for the neighbor when we last encountered it (or "infinity" / usize::MAX
+            // if we have not encountered the neighbor before)). If the new distance is less than
+            // the old one, record the new path as the tentative shortest path for the neighbor.
             for neighbor in &unvisited_neighbors {
                 let existing_neighbor_cost = shortest_paths
                     .get(neighbor)
                     .map(|v| v.len())
                     .unwrap_or(usize::MAX);
 
-                let mut neighbor_path = shortest_paths.get(neighbor).cloned().unwrap_or_default();
-
-                if neighbor_cost < existing_neighbor_cost {
-                    neighbor_path = current_path.clone();
+                if neighbor_distance < existing_neighbor_cost {
+                    let mut neighbor_path = current_path.clone();
                     neighbor_path.push(*neighbor);
-                    shortest_paths.insert(*neighbor, neighbor_path.clone());
+                    shortest_paths.insert(*neighbor, neighbor_path);
                 }
-
-                possible_paths.push(neighbor_path);
             }
 
-            possible_paths.sort_unstable_by(|p1, p2| match p1.len().cmp(&p2.len()) {
-                std::cmp::Ordering::Equal => p1.last().unwrap().cmp(p2.last().unwrap()),
-                v => v,
-            });
+            // Consider the current point to be "visited". The shortest path recorded for this
+            // point is now final.
+            unvisited.remove(&current);
 
-            if let Some(next) = possible_paths.first() {
-                unvisited.remove(&current);
-                current = *next.last().unwrap();
+            // Dijkstra's algorithm says to set the current point to the point that is closest
+            // to the starting position that has not yet been visited.
+
+            // Collect all recorded destinations that have a recorded path (this will include
+            // both visited points and unvisited neighbors of visited points).
+            let all_destinations = shortest_paths.keys().cloned().collect::<HashSet<_>>();
+
+            // Collect all of the recorded destinations that have not yet been visited.
+            let unvisited_destinations = all_destinations
+                .intersection(&unvisited)
+                .cloned()
+                .collect::<HashSet<_>>();
+
+            // Calculate the distance to the closest unvisited destination.
+            let closest_distance = unvisited_destinations
+                .iter()
+                .map(|v| shortest_paths[v].len())
+                .min();
+
+            // A number of unvisited destinations could have the same minimum distance so we sort
+            // them into reading order and select the first one as the next current point.
+            if let Some(closest_distance) = closest_distance {
+                let mut closest_destinations = unvisited_destinations
+                    .into_iter()
+                    .filter(|p| shortest_paths[p].len() == closest_distance)
+                    .collect::<Vec<_>>();
+                closest_destinations.sort_unstable();
+                current = *closest_destinations.first().unwrap();
             } else {
-                // We are finished. At this point, we have calculated a shortest path to every
-                // empty point on the grid. The "unvisited" set may not (and probably will not)
-                // be empty, however, as we may never have decided to move to those remaining
-                // points (there were cheaper alternatives).
+                // There are no more points on the grid that it is possible to move to. We're done.
                 break;
             }
         }
@@ -326,55 +390,80 @@ impl Game {
         shortest_paths
     }
 
-    fn try_attack(&mut self, point: Point) -> bool {
-        let subject = self.grid[&point].unwrap();
+    fn try_attack(&mut self, point: Point) -> AttackResult {
+        let subject = self.grid[&point].as_combatant();
         if let Some(enemy_point) = self.enemies_in_range(point).first() {
-            println!("Attacking {:?}", enemy_point);
-
-            let mut enemy = *self.grid[enemy_point].unwrap();
+            let mut enemy = *self.grid[enemy_point].as_combatant();
             enemy.health = enemy.health.saturating_sub(subject.attack_power);
+
             if enemy.health == 0 {
                 self.grid.insert(*enemy_point, Object::Empty);
+                AttackResult::Killed(*enemy_point)
             } else {
                 self.grid.insert(*enemy_point, Object::Combatant(enemy));
+                AttackResult::Hit(*enemy_point)
             }
-
-            true
         } else {
-            false
+            AttackResult::Missed
         }
     }
 
-    fn outcome(&self) -> GameOutcome {
+    fn outcome(&self) -> GameState {
         // Calculate the number of remaining health points for each kind of combatant.
-        let mut points = HashMap::new();
+        let mut remaining_health =
+            HashMap::from([(CombatantKind::Elf, 0), (CombatantKind::Goblin, 0)]);
+
+        // We'll also calculate the number of remaining combatants on each side.
+        let mut remaining_combatants = remaining_health.clone();
+
         for subject_point in self.combatant_points() {
-            let subject = *self.grid[&subject_point].unwrap();
-            let points = points.entry(subject.kind).or_insert(0);
+            let subject = *self.grid[&subject_point].as_combatant();
+            let points = remaining_health.entry(subject.kind).or_insert(0);
             *points += subject.health;
+
+            let remaining = remaining_combatants.entry(subject.kind).or_insert(0);
+            *remaining += 1;
         }
 
-        // If only one side has a score greater than zero (i.e., they're still alive) then
-        // the game is complete and we have a winner.
-        let game_complete = points.values().filter(|&v| *v > 0).count() == 1;
+        // If one side has zero remaining health then they are the loser and the the other
+        // side must be the winner.
+        let winner = remaining_health.iter().find_map(|(&kind, &health)| {
+            if health == 0 {
+                Some(kind.enemy())
+            } else {
+                None
+            }
+        });
 
-        let score = Score { result: points, rounds: self.rounds };
-        if game_complete {
-            GameOutcome::Complete(score)
+        let score = Score {
+            remaining_combatants,
+            total_health: remaining_health,
+            rounds_played: self.rounds,
+            winner,
+        };
+
+        // If we have a winner the game is over.
+        if winner.is_some() {
+            GameState::Complete(score)
         } else {
-            GameOutcome::InProgress(score)
+            GameState::InProgress(score)
         }
     }
 
-    fn play_round(&mut self) -> GameOutcome {
-        println!("{}", self);
+    fn play_round(&mut self) -> GameState {
+        // For each round, keep track of the points on the grid where a combatant was killed.
+        let mut kill_points = HashSet::new();
 
         // Iterate over each combatant in reading order.
         for subject_point in self.combatant_points() {
-            println!("Subject point is {:?}", subject_point);
+            if kill_points.contains(&subject_point) {
+                // At the beginning of the round there was a combatant at the current subject
+                // point but they have been killed and removed from the board so we continue.
+                continue;
+            }
 
             // Get the subject combatant at the current point.
-            let subject = *self.grid[&subject_point].unwrap();
+            let subject = *self.grid[&subject_point].as_combatant();
 
             // Get the list of enemy points. If there are none then the game has been won by
             // the side of the current subject so we return immediately.
@@ -384,9 +473,14 @@ impl Game {
             }
 
             // If the combatant is in a position to attack they do so and don't move.
-            if self.try_attack(subject_point) {
-                continue;
-            }
+            match self.try_attack(subject_point) {
+                AttackResult::Missed => (),
+                AttackResult::Hit(_) => continue,
+                AttackResult::Killed(p) => {
+                    kill_points.insert(p);
+                    continue;
+                }
+            };
 
             // Possible destinations for the subject to move towards are all empty points in range
             // of an enemy combatant as these are the points from which an attack can be launched.
@@ -399,44 +493,48 @@ impl Game {
                 possible_destinations.append(&mut attack_points);
             }
 
-            println!("Found {} possible destinations for subject", possible_destinations.len());
-
             // Calculate the shortest paths from the subject's position to every empty point.
             let shortest_paths = self.shortest_paths(subject_point);
-
-            println!("Calculated {} shortest paths", shortest_paths.len());
 
             // Choose the path to one of the previously calculated possible destinations (i.e.,
             // the attack positions) that will require the smallest number of moves.
             let mut chosen_path: Option<&Vec<Point>> = None;
             for destination in possible_destinations {
-                let path = &shortest_paths[&destination];
-                if path.len() < chosen_path.map(|v| v.len()).unwrap_or(usize::MAX) {
-                    chosen_path = Some(path);
+                if let Some(path) = shortest_paths.get(&destination) {
+                    if path.len() < chosen_path.map(|v| v.len()).unwrap_or(usize::MAX) {
+                        chosen_path = Some(path);
+                    }
                 }
             }
 
             // If a path was found, take the first step.
             if let Some(chosen_path) = chosen_path {
-                println!("Moving {:?} to {:?}", subject_point, chosen_path[0]);
-                self.grid.insert(chosen_path[0], Object::Combatant(subject));
                 self.grid.insert(subject_point, Object::Empty);
+
+                let subject_point = chosen_path[0];
+                self.grid.insert(subject_point, Object::Combatant(subject));
 
                 // If the path was only a single step in length then we must have arrived at
                 // the attack position so we launch an attack.
                 if chosen_path.len() == 1 {
-                    self.try_attack(subject_point);
+                    if let AttackResult::Killed(p) = self.try_attack(subject_point) {
+                        kill_points.insert(p);
+                    }
                 }
-            } else {
-                println!("Didn't mmove anywhere");
             }
-
-            println!("{}", self);
-            println!();
         }
 
         self.rounds += 1;
         self.outcome()
+    }
+
+    fn update_attack_power(&mut self, attack_powers: HashMap<CombatantKind, usize>) {
+        for point in self.combatant_points() {
+            let combatant = self.grid.get_mut(&point).unwrap().as_combatant_mut();
+            combatant.attack_power = *attack_powers
+                .get(&combatant.kind)
+                .unwrap_or(&Combatant::DEFAULT_ATTACK_POWER);
+        }
     }
 }
 
@@ -484,4 +582,44 @@ impl Display for Game {
 
         f.write_str(buf.as_str())
     }
+}
+
+#[derive(Clone, Debug)]
+enum GameState {
+    InProgress(Score),
+    Complete(Score),
+}
+
+impl GameState {
+    fn score(&self) -> &Score {
+        match self {
+            GameState::InProgress(score) => score,
+            GameState::Complete(score) => score,
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
+struct Score {
+    remaining_combatants: HashMap<CombatantKind, usize>,
+    total_health: HashMap<CombatantKind, usize>,
+    rounds_played: usize,
+    winner: Option<CombatantKind>,
+}
+
+impl Score {
+    fn outcome(&self) -> usize {
+        if let Some(winner) = self.winner {
+            self.total_health[&winner] * self.rounds_played
+        } else {
+            0
+        }
+    }
+}
+
+#[derive(Clone, Copy, Debug)]
+enum AttackResult {
+    Missed,
+    Hit(Point),
+    Killed(Point),
 }
